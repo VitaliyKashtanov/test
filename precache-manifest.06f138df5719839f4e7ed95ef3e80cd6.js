2bb57c4472b51e4459feae256275549d788b3b3e
Your Name <you@example.com>
2018-10-17 11:37:13 +0300

    test_project

---
+++ precache-manifest.06f138df5719839f4e7ed95ef3e80cd6.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "cb1bee900dbf8c84a1a48325d4f811a6",
+    "url": "/static/media/cat.cb1bee90.png"
+  },
+  {
+    "revision": "d4a21d9f559c441448801e957544d58e",
+    "url": "/static/media/background.d4a21d9f.jpg"
+  },
+  {
+    "revision": "44bde8744ccc0de7995a23b0e82dd281",
+    "url": "/static/media/Exo2.0-Thin.44bde874.otf"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "0e6a1c4c353b6fac3048",
+    "url": "/static/js/main.0e6a1c4c.chunk.js"
+  },
+  {
+    "revision": "ace62ad78903889e8e0e",
+    "url": "/static/js/1.ace62ad7.chunk.js"
+  },
+  {
+    "revision": "0e6a1c4c353b6fac3048",
+    "url": "/static/css/main.a59444ff.chunk.css"
+  },
+  {
+    "revision": "eab5a1a6cd4bf90bc9a19f5d73b7a5d1",
+    "url": "/index.html"
+  }
+];
